bb768019c71b931d9a6328a18724e5abcb762089
Priyadarshini Tamilselvan <priya61197@gmail.com>
2024-09-07 12:38:11 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',  // Tells Next.js to export a static site
+  basePath: '',      // Optional: base path for subfolder hosting
+  images: {
+    unoptimized: true,  // Disable image optimization for GitHub Pages
+  },
   reactStrictMode: true,
-}
+};
 
 module.exports = nextConfig
